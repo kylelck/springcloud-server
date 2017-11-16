c77daf765f0aea8f5945c44189b3b89c9cd21014
lichengkai <lick@hsyuntai.com>
2017-11-16 16:58:58 +0800

    Initial commit

---
+++ src/main/java/com/lick/SpringcloudServerApplication.java
@@ -0,0 +1,12 @@
+package com.lick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudServerApplication.class, args);
+	}
+}
